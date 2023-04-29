9e8b2e756c248ad90a21761e2b04840e32482bfd
Sebastian Itamari <sebaton66@gmail.com>
2023-04-29 19:53:38 -0400

    P3-001 Creating the structure of Patient class.

--- CoreLogic/Models/Patient.cs
+++ CoreLogic/Models/Patient.cs
@@ -2,5 +2,8 @@ namespace UPB.CoreLogic.Models;
 
 public class Patient
 {
-    
+    public int CI { get; set; }
+    public String Name { get; set; }
+    public String LastName { get; set; }
+    public String Group { get; set; }
 }

99937a6548ec1f51b51d3bfa4c0e6bd434b4b57e
Sebastian Itamari <sebaton66@gmail.com>
2023-04-29 20:22:24 -0400

    P3-001 Defining constructor of the PatientManager class.

--- CoreLogic/Managers/PatientManager.cs
+++ CoreLogic/Managers/PatientManager.cs
@@ -4,5 +4,10 @@ namespace UPB.CoreLogic.Managers;
 
 public class PatientManager
 {
-    
+    private List<Patient> _patients;
+
+    public PatientManager()
+    {
+        _patients = new List<Patient>();
+    }
 }

53e721d34ffe9f23c6807794c89184e1afa7e710
Sebastian Itamari <sebaton66@gmail.com>
2023-04-29 20:34:49 -0400

    P3-001 Defining function Create of the PatientManager class.

--- CoreLogic/Managers/PatientManager.cs
+++ CoreLogic/Managers/PatientManager.cs
@@ -10,4 +10,37 @@ public class PatientManager
     {
         _patients = new List<Patient>();
     }
+
+    public String GenerateGruop()
+    {
+        Random random = new Random();
+        int randomNumber = random.Next(0,8);
+
+        switch(randomNumber)
+        {
+            case 0:
+                return "A+";
+            case 1:
+                return "B+";
+            case 2:
+                return "AB+";
+            case 3:
+                return "O+";
+            case 4:
+                return "A-";
+            case 5:
+                return "B-";
+            case 6:
+                return "AB-";
+            default:
+                return "O-";
+        }
+    }
+
+    public Patient Create(String name, String lastName, int ci)
+    {
+        Patient createdPatient = new Patient(){ Name = name, LastName = lastName, CI = ci, Group = GenerateGruop() };
+        _patients.Add(createdPatient);
+        return createdPatient;
+    }
 }

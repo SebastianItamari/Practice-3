3ed553b1aef16f9eaf25cc9a95f0180ce60aec07
Sebastian Itamari <sebaton66@gmail.com>
2023-04-29 22:38:20 -0400

    P3-001 Adding HTTP methods information logs.

--- PracticeThree/Controllers/PatientController.cs
+++ PracticeThree/Controllers/PatientController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using UPB.CoreLogic.Managers;
 using UPB.CoreLogic.Models;
+using Serilog;
 
 namespace UPB.PracticeThree.Controllers;
 
@@ -9,14 +10,17 @@ namespace UPB.PracticeThree.Controllers;
 public class PatientController : ControllerBase
 {
     private readonly PatientManager _patientManager;
-    public PatientController(PatientManager patientManager)
+    private readonly ILogger<PatientController> _logger;
+    public PatientController(PatientManager patientManager, ILogger<PatientController> logger)
     {
         _patientManager = patientManager;
+        _logger = logger;
     }
 
     [HttpPost]
     public Patient Post([FromBody]Patient patientToCreate)
     {
+        _logger.LogInformation("Execute HTTP POST");
         return _patientManager.Create(patientToCreate.Name, patientToCreate.LastName, patientToCreate.CI);
     }
 
@@ -24,6 +28,7 @@ public class PatientController : ControllerBase
     [Route("{ci}")]
     public Patient Put([FromRoute] int ci, [FromBody]Patient patientToUpdate)
     {
+        _logger.LogInformation("Execute HTTP PUT");
         return _patientManager.Update(ci, patientToUpdate.Name, patientToUpdate.LastName);
     }
 
@@ -31,12 +36,14 @@ public class PatientController : ControllerBase
     [Route("{ci}")]
     public Patient Delete([FromRoute] int ci)
     {
+        _logger.LogInformation("Execute HTTP DELETE");
         return _patientManager.Delete(ci);
     }
 
     [HttpGet]
     public List<Patient> Get()
     {
+        _logger.LogInformation("Execute HTTP GET");
         return _patientManager.GetAll();  
     }
 
@@ -44,6 +51,7 @@ public class PatientController : ControllerBase
     [Route("{ci}")]
     public Patient GetById([FromRoute] int ci)
     {
+        _logger.LogInformation("Execute HTTP GET by CI");
         return _patientManager.GetById(ci);  
     }
 }
